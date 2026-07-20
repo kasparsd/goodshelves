b6b94c6c798653320632004d7011fbdf05506d29
kasparsd <kasparsd@users.noreply.github.com>
2026-07-20 15:19:43 +0000

    Release 1.0.0

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'kasparsd/goodshelves',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '9130b6582de98cf478c6f5ad1acbddf2981bfb7c',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'kasparsd/goodshelves' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '9130b6582de98cf478c6f5ad1acbddf2981bfb7c',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
