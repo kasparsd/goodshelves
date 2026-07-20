abaccf1d83308943af1f0a09908a2cef8a7d81fc
Kaspars Dambis <hi@kaspars.net>
2026-07-20 13:49:08 +0300

    match the return types

--- php/Goodreads/FeedApi.php
+++ php/Goodreads/FeedApi.php
@@ -2,11 +2,13 @@
 
 namespace Preseto\Goodshelves\Goodreads;
 
+use SimplePie\SimplePie;
+
 class FeedApi {
 
-	public function user_review_list( $user_id, $shelf ) {
+	public function user_review_list( int $user_id, string $shelf ): SimplePie {
 		$url = $this->url(
-			sprintf( 'review/list_rss/%d', intval( $user_id ) ),
+			sprintf( 'review/list_rss/%d', absint( $user_id ) ),
 			[
 				'shelf' => $shelf,
 			]
@@ -15,14 +17,17 @@ class FeedApi {
 		return $this->get( $url );
 	}
 
-	protected function get( $url ) {
-		return fetch_feed( $url );
+	protected function get( string $url ): SimplePie {
+		$feed = fetch_feed( $url ); // Rely on 12h cache by WP core around this.
+
+		if ( is_wp_error( $feed ) ) {
+			throw new \RuntimeException( sprintf( 'Failed to fetch feed: %s', $feed->get_error_message() ) );
+		}
+
+		return $feed;
 	}
 
-	protected function url( $path, $params = [] ) {
-		// Remove empty attributes.
-		$params = array_filter( $params );
-
+	protected function url( string $path, array $params = [] ): string {
 		$url = sprintf(
 			'https://www.goodreads.com/%s',
 			trim( $path, '/' )
@@ -30,5 +35,4 @@ class FeedApi {
 
 		return add_query_arg( $params, $url );
 	}
-
 }
