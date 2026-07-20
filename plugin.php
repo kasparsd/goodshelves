60e51d5b4abc2f3168b55082cfe9a527db96676b
Kaspars Dambis <hi@kaspars.net>
2026-07-20 13:40:16 +0300

    load only if no global autoload is present

--- plugin.php
+++ plugin.php
@@ -5,7 +5,14 @@
 
 namespace Preseto\Goodshelves;
 
-include __DIR__ . '/vendor/autoload.php';
+if ( ! function_exists( 'add_action' ) ) {
+	return; // Ensure WP core is loading the plugin.
+}
+
+// Only if global project-wide autoload is not present.
+if ( is_readable( __DIR__ . '/vendor/autoload.php' ) ) {
+	require_once __DIR__ . '/vendor/autoload.php';
+}
 
 $plugin = new Plugin( __FILE__ );
 $goodshelve = new GoodshelvesPlugin( $plugin );
